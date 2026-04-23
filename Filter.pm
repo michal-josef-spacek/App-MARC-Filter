404ba0260d65c289a24963d0dd5a304a6994a1b6
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-04-23 19:46:54 +0200

    Prepare for 0.11 version

--- Filter.pm
+++ Filter.pm
@@ -20,7 +20,7 @@ use Unicode::UTF8 qw(encode_utf8 decode_utf8);
 Readonly::Array our @OUTPUT_FORMATS => qw(ascii xml);
 Readonly::Array our @CONTROL_FIELDS => qw(001 003 005 006 007 008);
 
-our $VERSION = 0.10;
+our $VERSION = 0.11;
 
 $| = 1;
 
@@ -653,6 +653,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.10
+0.11
 
 =cut
